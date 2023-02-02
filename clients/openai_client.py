db9b1b8ada17f78bcb0d6af349b8ab2e9ac55cdb
Divan Visagie <me@dvisagie.com>
2023-02-02 02:05:27 +0100

    Better error handling for openapi client

--- clients/openai_client.py
+++ clients/openai_client.py
@@ -9,15 +9,20 @@ model_engine = "text-davinci-003"
 logger = logging.getLogger(__name__)
 
 def get_answer(prompt):
-    logger.debug(f'Calling OpenAI completion with Prompt:\n{prompt}')
-    completion = openai.Completion.create(
-        model=model_engine,
-        prompt=prompt,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    logger.debug(f'Returned: {completion}')
-    message = completion.choices[0].text
-    return message
+    try:
+        logger.debug(f'Calling OpenAI completion with Prompt:\n{prompt}')
+        completion = openai.Completion.create(
+            model=model_engine,
+            prompt=prompt,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        logger.debug(f'Returned: {completion}')
+        message = completion.choices[0].text
+        return message
+    except Exception as e:
+        logger.error(f'Failed to get answer from OpenAI: {e}')
+        return 'A trickster seems to have disabled the Bifrost! We cannot reach the library of the gods.'
+
