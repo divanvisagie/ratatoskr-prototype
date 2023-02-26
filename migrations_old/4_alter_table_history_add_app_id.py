db07358609f55cae1d951c940ed00190c73bb60c
Divan Visagie <me@dvisagie.com>
2023-02-26 13:29:44 +0100

    Delete old migrations

--- migrations_old/4_alter_table_history_add_app_id.py
+++
@@ -1,9 +0,0 @@
-from executor import Executor
-
-ex = Executor()
-
-ex.commit("""
-ALTER TABLE history ADD COLUMN app_id INTEGER
-""")
-
-ex.release_resources()
--- migrations_old/README.md
+++
@@ -1,3 +0,0 @@
-A very home made, hacky migrations strategy
-
-Run the python scripts in their numbered order to set up the sql database
