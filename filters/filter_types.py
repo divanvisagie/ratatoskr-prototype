149a394385d312f6d87a8e70e1bf0990aab494cd
Divan Visagie <me@dvisagie.com>
2023-03-16 00:20:34 +0100

    Improve filtesr

--- filters/filter_types.py
+++ filters/filter_types.py
@@ -1,16 +1,44 @@
 from abc import ABC, abstractmethod
+from typing import List, Tuple
 
 from message_handler.message_types import RequestMessage, ResponseMessage
 
-
 class Filter (ABC):
-    description = "Fallback"
-  
-    def applies_to(self, msg: RequestMessage):
+    """Defines the interface for a filter"""
+    description = "Not Implemented" # If not implemented then will be ignored by fallback mechanism
+    
+    def applies_to(self, msg: RequestMessage) -> float:
         """Represents a filter that can be applied to a message"""
-        return True
+        return 0.00
     
     @abstractmethod
     def process(self, msg: RequestMessage) -> ResponseMessage:
         """Defines the logic to process the message"""
         pass
+
+
+def find_most_applicable(filters: List[Filter], message: RequestMessage) -> Tuple[Filter, float]:
+    """
+    Finds the most applicable filter in a list of filters
+
+    Args:
+        filters (List[Filter]): The list of filters to search
+        message (RequestMessage): The message to search for
+
+    Returns:
+        Tuple[Filter, float]: The most applicable filter and the confidence of the filter's applicability
+    """
+    current_best = None
+    current_best_applicability = 0.0
+    for filter in filters:
+        if current_best is None:
+            current_best = filter
+            current_best_applicability = filter.applies_to(message)
+        else:
+            applicability = filter.applies_to(message)
+            if applicability > current_best_applicability:
+                current_best = filter
+                current_best_applicability = applicability
+
+    return (current_best, current_best_applicability)
+
---
+++ tests/unit/test_filter.py
@@ -0,0 +1,27 @@
+
+from filters.filter_types import Filter, find_most_applicable
+from message_handler.message_types import RequestMessage, ResponseMessage
+
+
+class MockFilter(Filter):
+    def __init__(self, application: float):
+        self.application = application
+
+    def applies_to(self, msg: RequestMessage) -> float:
+        return self.application
+    
+    def process(self, msg: RequestMessage) -> ResponseMessage:
+        return ResponseMessage('Filter1')
+
+def applicability_case(filters, expected_filter, expected_confidence):
+    m = RequestMessage('mock message', 'mock user')
+    actual_filter, actual_confidence = find_most_applicable(filters, m)
+    assert actual_filter == expected_filter
+    assert actual_confidence == expected_confidence
+
+def test_find_most_applicable():
+    highest = MockFilter(0.9)
+    # Test cases where order is different to ensure we aren't picking by order
+    applicability_case([highest, MockFilter(0.8), MockFilter(0.5)], highest, 0.9)
+    applicability_case([MockFilter(0.8), highest, MockFilter(0.5)], highest, 0.9)
+    applicability_case([MockFilter(0.8), MockFilter(0.5), highest], highest, 0.9)
