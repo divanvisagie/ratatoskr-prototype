21825f5f6887042377bc672419ce10daff119494
Divan Visagie <me@dvisagie.com>
2023-03-15 21:03:07 +0100

    all green

--- tests/model/test_named_model.py
+++
@@ -1,9 +0,0 @@
-
-from language_model.named_transformers_model import NamedModel
-
-
-def test_gpt2_instruct_model():
-    model_name = "SummerSigh/GPT2-Instruct-SFT"
-    model = NamedModel(model_name)
-    actual = model.complete("What search term would I use if I wanted to find the akka library for scala")
-    assert actual == "akka"
