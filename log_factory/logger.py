7ce813a6296eb35fc9fcd2958ffdfab6364a50b9
Divan Visagie <me@dvisagie.com>
2023-03-24 20:08:04 +0100

    pass in name

--- log_factory/logger.py
+++ log_factory/logger.py
@@ -12,7 +12,7 @@ def create_logger(name: str) -> logging.Logger:
     logging.basicConfig(
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
     )
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(name)
     logger.addHandler(file_handler)
     logger.setLevel(logging.INFO)
     return logger
