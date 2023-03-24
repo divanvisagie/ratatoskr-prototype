4ff7b05bfcd914a4a7d83182a211cbbb07c6fe1e
Divan Visagie <me@dvisagie.com>
2023-03-24 18:46:45 +0100

    Put bot into its own interface

---
+++ interfaces/telegram_bot.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from os import environ
+import logging
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from message_handler.message_handler import handle_incoming_telegram_message
+
+TELEGRAM_BOT_TOKEN = environ['TELEGRAM_BOT_TOKEN']
+
+async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Pong {update.effective_user.first_name}')
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+def main():
+    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    app.add_handler(CommandHandler('ping', ping))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_incoming_telegram_message))
+    logger.info('starting app..')
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
