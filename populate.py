8de1ee387133ecd21eb277633eac272e07e2b103
Divan Visagie <me@divanv.com>
2023-04-14 19:02:19 +0200

    Better error handling in population script

--- populate.py
+++ populate.py
@@ -1,9 +1,13 @@
 from repositories.user import NewUser, UserRepository
 
 def main():
-    repo = UserRepository()
-    user = NewUser("DivanVisagie", 0)
-    repo.save(user)
+    try:
+        repo = UserRepository()
+        user = NewUser("DivanVisagie", 0)
+        repo.save(user)
+        print("User saved")
+    except Exception as e:
+        print(e)       
 
 if __name__ == '__main__':
     main()
